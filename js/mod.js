9bf5a57b26ec9330896d7cb675997c18c557044e
Wings <112726672+Wings-the-kobold@users.noreply.github.com>
2026-04-08 00:10:27 -0800

    accidental swap

--- js/mod.js
+++ js/mod.js
@@ -397,12 +397,12 @@ function getPointGen() {
 	if (hasUpgrade("C",13)) gain = gain.times(DarknessUpgs_Row1[2] ** deAmp)
 	if (hasUpgrade("C",11)) gain = gain.times(DarknessUpgs_Row1[0] ** deAmp)
 
-	if ((player.Sol.TBCore.active && getCoreDifficulty().gte(3))) gain = gain.mul(GetHeirarchyBonus()) 
-		
+	if (!(player.Sol.TBCore.active && getCoreDifficulty().gte(3))) gain = gain.mul(GetHeirarchyBonus()) 
+
 	if (player.C.activeCheck == "Formality" || (player.Sol.TBCore.active && getCoreDifficulty().gte(3)) ) gain = gain.pow(0.666)
 	if (player["C"].activeCheck == "Twilight" || (player.Sol.TBCore.active && getCoreDifficulty().gte(3)) ) gain = gain.log(12)
 
-	if (!(player.Sol.TBCore.active && getCoreDifficulty().gte(3))) gain = gain.mul(GetHeirarchyBonus()) 
+	if ((player.Sol.TBCore.active && getCoreDifficulty().gte(3))) gain = gain.mul(GetHeirarchyBonus()) 
 	
 	if (hasUpgrade("C",22)) gain = gain.pow(1.15);
 
